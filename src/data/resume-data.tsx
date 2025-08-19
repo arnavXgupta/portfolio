4248cebda49ad2f3d82dfdc144709a00ba1a4563
Arnav Gupta <arnav090404@Gmail.com>
2025-08-19 11:20:25 +0530

    updt

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -57,21 +57,7 @@ export const RESUME_DATA: ResumeData = {
       title: "Freelance Web Developer",
       start: "May 2025",
       end: "July 2025",
-      description: (
-        <>
-          <ul className="list-inside list-disc">
-            <li>
-              Developed a major platform overhaul using Next.js and Tailwind CSS, implementing a clean and sleek UI that elevated the brand's premium aesthetic and improved user experience.
-            </li>
-            <li>
-              Developed and integrated a custom, high-performance blog using Sanity CMS, which directly led to a significant increase in user engagement and content interaction.
-            </li>
-            <li>
-              Engineered and implemented an RSS feed to syndicate content and expand audience reach, successfully growing the platform's user reach by an additional 30,000.
-            </li>
-          </ul>
-        </>
-      ),
+      description: "Led a major platform overhaul using Next.js and Tailwind CSS, integrating a custom Sanity CMS blog and an RSS feed to enhance UI/UX, boost user engagement, and expand audience reach by 30,000.",
     },
     {
       company: "NiveshX",
@@ -80,15 +66,8 @@ export const RESUME_DATA: ResumeData = {
       title: "Frontend Developer & UI/UX Designer",
       start: "May 2024",
       end: "July 2024",
-      description: (
-        <>
-          <ul className="list-inside list-disc">
-            <li>
-              Spearheaded the development of a comprehensive financial management platform, enhancing user experience and engagement through intuitive UI/UX design and seamless integration with Firebase for real-time data handling.
-            </li>
-          </ul>
-        </>
-      ),
+      description:
+        "Spearheaded the development of a comprehensive financial management platform, enhancing user experience and engagement through intuitive UI/UX design and seamless integration with Firebase for real-time data handling.",
     },
     {
       company: "Chemical Department, TIET",
@@ -130,43 +109,60 @@ export const RESUME_DATA: ResumeData = {
   projects: [
     {
       title: "TailorMate",
-      techStack: ["Java", "Object Oriented Programming", "JavaFX", "Scene Builder", "MySQL"],
+      techStack: ["Java", "Object Oriented Programming", "JavaFX", "Scene Builder", "MySQL", "Maven"],
       description:
-        "Led the development of a desktop application to streamline inefficient tailor shop operations, reducing order processing time by 30% and improving customer delivery accuracy by 25%.",
+        "TailorMate is a boutiques and tailor shop management system designed to streamline order management, track customer details, and optimize workflow.",
       link: {
         label: "github.com",
         href: "https://github.com/arnavXgupta/TailorMate",
       },
     },
     {
-      title: "AI-Powered Resume Builder",
-      techStack: ["React.js", "Node.js", "Express.js", "MongoDB", "Tailwind CSS"],
-      description:
-        "Developed an AI-powered resume builder that streamlines the resume creation process by leveraging natural language processing and machine learning algorithms.",
-      link: {
-        label: "github.com",
-        href: "https://github.com/arnavXgupta/AI-Powered-Resume-Builder",
-      },
-    },
-    {
       title: "DocuPrism RAG",
       techStack: ["Python", "FastAPI", "Pinecone", "OpenAI", "Gemini", "LangChain"],
       description:
-        "Developed a scalable, production-ready RAG API capable of handling concurrent queries by leveraging a fully asynchronous architecture for parallelized retrieval and generation tasks.",
+        "This project is a high-performance, scalable Retrieval-Augmented Generation (RAG) pipeline designed to answer questions about complex documents provided via a URL.",
       link: {
         label: "github.com",
         href: "https://github.com/arnavXgupta/Bajaj-RAG",
       },
     },
     {
+      "title": "IntelliMatch",
+      "techStack": ["Python", "Openai", "Gemini"],
+      "description": "IntelliMatch is an intelligent recruitment platform that automates the process of shortlisting top candidates by using AI to score and rank them based on how well their resumes match job descriptions.",
+      "link": {
+        "label": "github.com",
+        "href": "https://github.com/arnavXgupta/IntelliMatch"
+      }
+    },
+    {
       title: "Autonomous Pothole Detection & 3D Reconstruction",
       techStack: ["YOLO", "LiDAR", "Transformers", "ICP"],
       description:
-        "Partnered with a team of 4 on developing the AI-driven software pipeline for a drone-assisted pothole detection and mapping system, aiming to significantly reduce municipal costs and improve infrastructure management.",
+        "Our capstone team of four is developing a real-time autonomous system that fuses LiDAR and RGB camera data to accurately detect and map potholes, enhancing road safety.",
       link: {
         label: "github.com",
         href: "https://github.com/arnavXgupta/",
       },
+    },
+    {
+      "title": "Stock Analysis",
+      "techStack": ["Python", "Pandas", "AlphaVantage", "Plotly"],
+      "description": "A comprehensive stock analysis tool that provides real-time data, interactive visualizations, and key financial metrics for any publicly traded company in the US. It analyses based on the indicators: SMA, EMA, MACD, Stochastic Oscillators, Boillinger Bands and RSI",
+      "link": {
+        "label": "github.com",
+        "href": "httpsT://github.com/arnavxgupta/stock-analysis"
+      }
+    },
+    {
+      "title": "SmartShop AI",
+      "techStack": ["Python", "FastAPI", "OpenAI", "Pandas", "Streamlit"],
+      "description": "An AI-powered Walmart shopping assistant that provides personalized product recommendations based on user queries.",
+      "link": {
+        "label": "github.com",
+        "href": "https://github.com/arnavxgupta/smartshop-ai"
+      }
     },
   ],
 } as const;
